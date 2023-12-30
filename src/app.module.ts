dc7f6a5588037bd83c07ff65a8778c1a96baffe2
Daniel Cruz <danielcunha54@gmail.com>
2023-12-29 22:17:08 -0300

    Add controller for authentication with @nestjs/jwt

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
 
+import { AuthModule } from './auth/auth.module'
 import { CreateAccountController } from './controllers/create-account.controller'
-import { PrismaService } from './prisma/prisma.service'
+import { CreateSessionController } from './controllers/create-session.controller'
 import { envSchema } from './env'
+import { PrismaService } from './prisma/prisma.service'
 
 @Module({
   imports: [
@@ -11,8 +13,9 @@ import { envSchema } from './env'
       validate: (env) => envSchema.parse(env),
       isGlobal: true,
     }),
+    AuthModule,
   ],
-  controllers: [CreateAccountController],
+  controllers: [CreateAccountController, CreateSessionController],
   providers: [PrismaService],
 })
 export class AppModule {}
---
+++ src/controllers/create-session.controller.ts
@@ -0,0 +1,48 @@
+import {
+  Body,
+  Controller,
+  Post,
+  UnauthorizedException,
+  UsePipes,
+} from '@nestjs/common'
+import { z } from 'zod'
+import { compare } from 'bcryptjs'
+
+import { JwtService } from '@nestjs/jwt'
+import { ZodValidationPipe } from 'src/pipes/zod-validation-pipe'
+import { PrismaService } from 'src/prisma/prisma.service'
+
+const bodySchema = z.object({
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+type RequestBody = z.infer<typeof bodySchema>
+
+@Controller('/sessions')
+export class CreateSessionController {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jwt: JwtService,
+  ) {}
+
+  @Post()
+  @UsePipes(new ZodValidationPipe(bodySchema))
+  async handle(@Body() body: RequestBody) {
+    const { email, password } = body
+    const user = await this.prisma.user.findUnique({ where: { email } })
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid credentials.')
+    }
+
+    const doesPasswordMatch = await compare(password, user.password)
+    if (!doesPasswordMatch) {
+      throw new UnauthorizedException('Invalid credentials.')
+    }
+
+    const accessToken = this.jwt.sign({}, { subject: user.id })
+
+    return { accessToken }
+  }
+}
